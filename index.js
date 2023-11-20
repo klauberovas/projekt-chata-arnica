e4887185b818410332ffdae9b3dac5e1c8f89153
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-20 11:33:52 +0100

    Done

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
-
+// vybrání divu s přepínačem
 const toggleSwitch = document.querySelector(
   '.theme-switch input[type="checkbox"]',
 );
 
+//fce pro přepínání
 const switchTheme = (e) => {
   if (e.target.checked) {
+    ////checked true = použijí se barvy z dark
     document.documentElement.setAttribute('data-theme', 'dark');
+    // použijí se původně nastavené hodnoty
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
   }
 };
 
+// Posluchač navěšený na div při změně se spustí fce
 toggleSwitch.addEventListener('change', switchTheme, false);
